1c6545fbc8d318e7adc1d68d4d8649c91931e232
rashardj34 <96150491+rashardj34@users.noreply.github.com>
2022-08-24 20:41:47 -0400

    calc config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+function findRequest(){
+  var invoiceOne = document.getElementById("firstInvoice").value;
+  var invoiceOne = parseInt(invoiceOne, 10);
+  var invoiceTwo = document.getElementById("secondInvoice").value;
+  var invoiceTwo = parseInt(invoiceTwo, 10);
+  var invoiceThree = document.getElementById("secondInvoice").value;
+  var invoiceThree = parseInt(invoiceThree, 10);
+  const max = document.getElementById("increment").value;
+  const fee = document.getElementById("fee").value;
+  const requestFirst = (Math.ceil(invoiceOne / max) * fee) - fee;
+  const requestSecond = (Math.ceil(invoiceTwo / max) * fee);
+  const requestThird = (Math.ceil(invoiceThree / max) * fee);
+  const requestFee = requestFirst + requestSecond + requestThird;
+  
+console.log(requestFee);
+  document.getElementById("requestAmount").value = Math.round(requestFee * 100) /100;      
+  }
+
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+return (
+<div className="App">
+ <div className="calculator">
+   <h1>Fee Calculator</h1>
+   <form action="" method="get" className="invoices">
+     <div className="invoiceOne">
+       <label for="firstInvoice">1st Invoice: </label>
+       <input type="text" name="firstInvoice" id="firstInvoice" className='invoice' required></input>
+     </div>
+     <div className="invoiceTwo">
+       <label for="secondInvoice">2nd Invoice: </label>
+       <input type="text" name="secondInvoice" id="secondInvoice" className='invoice'  required></input>
+     </div>
+     <div className="invoiceThree">
+       <label for="thirdInvoice">3rd Invoice: </label>
+       <input type="text" name="thirdInvoice" id="thirdInvoice" className='invoice'  required></input>
+     </div>
+
+   </form>
+   <div className='transaction'>
+    <label for="increment">Max: </label>
+    <input type="text" name="increment" id='increment' required></input><br></br>
+    <label for="fee">Fee: </label>
+    <input type="text" name="fee" id="fee" required></input><br></br>
+    <label for="toRequest">Request: </label>
+    <input type="number" for="request" name="request" id="requestAmount" required></input>
+    <br></br>
+     <button onClick={findRequest}>Calculate</button> 
+    
+    <button>Reset</button>
+   </div>
+ </div>
+</div>
+);
 }
 
 export default App;
