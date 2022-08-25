7c3b2dec20aa59f90a7e4bc0dbcbd9dcf668500d
rashardj34 <96150491+rashardj34@users.noreply.github.com>
2022-08-24 20:51:30 -0400

    input fix

--- src/App.js
+++ src/App.js
@@ -34,15 +34,15 @@ return (
    <form action="" method="get" className="invoices">
      <div className="invoiceOne">
        <label for="firstInvoice">1st Invoice: </label>
-       <input type="text" name="firstInvoice" id="firstInvoice" className='invoice' required></input>
+       <input type="text" name="firstInvoice" id="firstInvoice" className='invoice'value='0'  required></input>
      </div>
      <div className="invoiceTwo">
        <label for="secondInvoice">2nd Invoice: </label>
-       <input type="text" name="secondInvoice" id="secondInvoice" className='invoice'  required></input>
+       <input type="text" name="secondInvoice" id="secondInvoice" className='invoice' value='0'  required></input>
      </div>
      <div className="invoiceThree">
        <label for="thirdInvoice">3rd Invoice: </label>
-       <input type="text" name="thirdInvoice" id="thirdInvoice" className='invoice'  required></input>
+       <input type="text" name="thirdInvoice" id="thirdInvoice" className='invoice' value='0'  required></input>
      </div>
 
    </form>
