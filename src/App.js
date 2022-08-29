2c4470243a1ecc1d8331d0bdb3759c5e7e2ce3d8
rashardj34 <96150491+rashardj34@users.noreply.github.com>
2022-08-28 20:35:10 -0400

    input fix

--- src/App.js
+++ src/App.js
@@ -33,16 +33,16 @@ return (
    <h1>Fee Calculator</h1>
    <form action="" method="get" className="invoices">
      <div className="invoiceOne">
-       <label for="firstInvoice">1st Invoice: </label>
-       <input type="text" name="firstInvoice" id="firstInvoice" className='invoice'value='0'  required></input>
+       <label for="firstInvoice">1st Invoice Amount: </label>
+       <input type="text" name="firstInvoice" id="firstInvoice" className='invoice' placeholder='0'  required></input>
      </div>
      <div className="invoiceTwo">
-       <label for="secondInvoice">2nd Invoice: </label>
-       <input type="text" name="secondInvoice" id="secondInvoice" className='invoice' value='0'  required></input>
+       <label for="secondInvoice">2nd Invoice Amount: </label>
+       <input type="text" name="secondInvoice" id="secondInvoice" className='invoice' placeholder='0'  required></input>
      </div>
      <div className="invoiceThree">
-       <label for="thirdInvoice">3rd Invoice: </label>
-       <input type="text" name="thirdInvoice" id="thirdInvoice" className='invoice' value='0'  required></input>
+       <label for="thirdInvoice">3rd Invoice Amount: </label>
+       <input type="text" name="thirdInvoice" id="thirdInvoice" className='invoice' placeholder='0'  required></input>
      </div>
 
    </form>
